d9cde717587369f7866376f2140cd5e2e76d0b63
Elliot Chavez <elliot.chavez@openmarket.com>
2020-06-05 12:08:56 -0700

    fix references in demo

--- projects/demo-app/src/app/app.module.ts
+++ projects/demo-app/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './demo-async/in-memory-data.service';
 
 import { AppComponent } from './app.component';
-import { MentionModule } from 'angular-mentions';
 import { DemoAsyncComponent } from './demo-async/demo-async.component';
 import { DemoConfigComponent } from './demo-config/demo-config.component';
 import { DemoEventsComponent } from './demo-events/demo-events.component';
@@ -15,6 +14,7 @@ import { DemoOptionsComponent } from './demo-options/demo-options.component';
 import { DemoTemplateComponent } from './demo-template/demo-template.component';
 import { DemoTinymceComponent } from './demo-tinymce/demo-tinymce.component';
 import { TestPositionComponent } from './test-position/test-position.component';
+import {MentionModule} from '../../../angular-mentions/src/lib';
 
 @NgModule({
   imports: [
